eb3474040b2eceeb30c5e908b4b3866e8f5fc0c1
Drew A <alvarez.drew@gmail.com>
2020-11-13 23:10:17 -0600

    index.js schema, api-routes updates, got page to create workout db on startup

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,8 +1,24 @@
-//const db = require("../models");
+const Workout = require("../models");
 const mongoose = require("mongoose");
 
 module.exports = function(app){
-    app.get("/api/workout", function(req, res){
-        res.json(req.workouts)
-    })
+    app.get("/api/workouts", function(req, res){
+        Workout.find({})
+        .then(function(dbWorkout){
+            res.json(dbWorkout);
+        })
+        .catch(function(err){
+            res.status(400).json(err);
+        });        
+    });
+
+    app.post("/api/workouts", function({ body }, res){
+        Workout.create(body)
+        .then(function(dbWorkout){
+            res.json(dbWorkout);
+        })
+        .catch(function(err){
+            res.status(400).json(err);
+        });
+    });
 }
