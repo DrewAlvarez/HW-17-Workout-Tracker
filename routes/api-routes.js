4f408ed2e5f162889fcbe399f0600ba0caaaf0a6
Drew A <alvarez.drew@gmail.com>
2020-11-15 09:05:23 -0600

    messing with api-routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,16 +2,6 @@ let db = require("../models");
 let mongoose = require("mongoose");
 
 module.exports = function(app){
-    app.get("/api/workouts", function(req, res){
-        db.Workout.find({})
-        .then(function(dbWorkout){
-            res.json(dbWorkout);
-        })
-        .catch(function(err){
-            res.status(400).json(err);
-        });        
-    });
-
     app.post("/api/workouts", function({ body }, res){
         db.Workout.create(body)
         .then(function(dbWorkout){
@@ -22,18 +12,28 @@ module.exports = function(app){
         });
     });
 
-    app.put("/api/workouts/:id", function({ params }, res){
-        db.Workout.updateOne({
-            _id: mongojs.ObjectId(params.id)
-        },
-        {
-            complete: params.complete
-        })
+    app.get("/api/workouts", function(req, res){
+        db.Workout.find({})
         .then(function(dbWorkout){
             res.json(dbWorkout);
         })
         .catch(function(err){
             res.status(400).json(err);
-        }); 
+        });        
     });
+
+    // app.put("/api/workouts/:id", function({ params }, res){
+    //     db.Workout.updateOne({
+    //         _id: mongojs.ObjectId(params.id)
+    //     },
+    //     {
+    //         complete: params.complete
+    //     })
+    //     .then(function(dbWorkout){
+    //         res.json(dbWorkout);
+    //     })
+    //     .catch(function(err){
+    //         res.status(400).json(err);
+    //     }); 
+    // });
 }
